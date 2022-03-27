5219f56f87aa2de559435a83f5995e09fd890855
Masum <mmmasumbillah@gmail.com>
2022-03-27 07:05:21 +0600

    added: route for to and sub todo

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.use(cors());
 const db = require("./models")
 // Routers
 
+const postRouter = require('./routes/Todos')
+app.use("/todos", postRouter)
+app.use("/todosStatus", postRouter)
+app.use("/subtasksStatus", postRouter)
+
+
+const subtasksRouter = require('./routes/Subtasks')
+app.use("/subtasks", subtasksRouter)
+
 db.sequelize.sync().then( () => {
     app.listen("4000", () => {
         console.log("Server running on port 4000")
